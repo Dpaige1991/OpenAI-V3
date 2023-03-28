36d02af184b3832de69ca70dc1705882e19c3932
Dominique D Paige <dominique.paige@outlook.com>
2023-03-28 16:46:22 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async(e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://openai-v3.onrender.com', {
         method: 'POST',
         headers: {
             'Content-type' : 'application/json',
